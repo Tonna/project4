4c3db5d8f4a22d13aa43d005ff8ee4c7cf44b78e
Anton Yakovchuk <anton_yakovchuk@epam.com>
2015-05-22 19:01:47 +0300

    added ids to test data

--- sphynx/src/main/java/com/yakovchuk/sphynx/dao/ExamDataProvider.java
+++ sphynx/src/main/java/com/yakovchuk/sphynx/dao/ExamDataProvider.java
@@ -12,14 +12,14 @@ public class ExamDataProvider {
         ArrayList<Question> questions1 = new ArrayList<Question>();
 
         ArrayList<Answer> answers11 = new ArrayList<Answer>();
-        answers11.add(new Answer.Builder("2", true).build());
-        answers11.add(new Answer.Builder("3", false).build());
-        questions1.add(new Question.Builder("1 + 1 = ?", answers11).build());
+        answers11.add(new Answer.Builder("2", true).id("11").build());
+        answers11.add(new Answer.Builder("3", false).id("12").build());
+        questions1.add(new Question.Builder("1 + 1 = ?", answers11).id("15").build());
 
         ArrayList<Answer> answers12 = new ArrayList<Answer>();
-        answers12.add(new Answer.Builder("1024", true).build());
-        answers12.add(new Answer.Builder("20", false).build());
-        questions1.add(new Question.Builder("2 ^ 10 = ?", answers12).build());
+        answers12.add(new Answer.Builder("1024", true).id("13").build());
+        answers12.add(new Answer.Builder("20", false).id("14").build());
+        questions1.add(new Question.Builder("2 ^ 10 = ?", answers12).id("16").build());
 
         return new Exam.Builder("Basic math", questions1).id("1").subject("Math").build();
     }
@@ -28,15 +28,15 @@ public class ExamDataProvider {
         ArrayList<Question> questions2 = new ArrayList<Question>();
 
         ArrayList<Answer> answers21 = new ArrayList<Answer>();
-        answers21.add(new Answer.Builder("Yes", false).build());
-        answers21.add(new Answer.Builder("No", true).build());
-        questions2.add(new Question.Builder("Is it enough boilerplate code?", answers21).build());
+        answers21.add(new Answer.Builder("Yes", false).id("17").build());
+        answers21.add(new Answer.Builder("No", true).id("18").build());
+        questions2.add(new Question.Builder("Is it enough boilerplate code?", answers21).id("19").build());
 
         ArrayList<Answer> answers22 = new ArrayList<Answer>();
-        answers22.add(new Answer.Builder("Yes", true).build());
-        answers22.add(new Answer.Builder("No", false).build());
+        answers22.add(new Answer.Builder("Yes", true).id("21").build());
+        answers22.add(new Answer.Builder("No", false).id("22").build());
         questions2.add(new Question.Builder("Maybe I should type less? Is there a way to develop software fast?",
-                answers22).build());
+                answers22).id("23").build());
 
         return new Exam.Builder("Despair exam", questions2).id("2").subject("Software development").build();
     }
