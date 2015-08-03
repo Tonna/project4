912165f14aa24d249a5fb1c3637cd85944b69a23
Anton Yakovchuk <anton_yakovchuk@epam.com>
2015-08-03 17:41:15 +0300

    simplification

--- sphynx/src/main/java/com/yakovchuk/sphynx/util/ExamHelper.java
+++ sphynx/src/main/java/com/yakovchuk/sphynx/util/ExamHelper.java
@@ -4,8 +4,6 @@ import com.yakovchuk.sphynx.domain.Answer;
 import com.yakovchuk.sphynx.domain.Exam;
 import com.yakovchuk.sphynx.domain.Question;
 
-import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.Map;
 
 public class ExamHelper {
@@ -14,16 +12,15 @@ public class ExamHelper {
     }
 
     private Exam buildQuestions(Map<String, String[]> parameterMap, Exam.Builder builder) {
-        Iterator<String> questions = parameterMap.keySet().stream().filter(key -> key.contains("answer-id-for-question")).iterator();
-        ArrayList questionsList = new ArrayList<Question>();
-        questions.forEachRemaining(q -> builder.addQuestion(buildQuestion(parameterMap, q)));
+        parameterMap.keySet()
+                .stream().filter(key -> key.contains("answer-id-for-question")).iterator().
+                forEachRemaining(q -> builder.addQuestion(buildQuestion(parameterMap, q)));
         return builder.build();
     }
 
     private Question buildQuestion(Map<String, String[]> parameterMap, String question) {
-        String[] answerIds = parameterMap.get(question);
         Question.Builder builder = new Question.Builder().id(question.replace("answer-id-for-question-id-", ""));
-        for (String answerId : answerIds) {
+        for (String answerId : parameterMap.get(question)) {
             builder.addAnswer(new Answer.Builder().id(answerId).build());
         }
         return builder.build();
