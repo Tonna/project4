85abbe63bab84c4592c9167077b231a78b61bcdb
Anton Yakovchuk <anton_yakovchuk@epam.com>
2015-09-15 11:29:47 +0300

    Moved sql strings to property file. Added that property file reading on startup. Property file added to classes folder on war step.

--- sphinx/src/main/java/com/yakovchuk/sphinx/initialization/InitializationServletContextListener.java
+++ sphinx/src/main/java/com/yakovchuk/sphinx/initialization/InitializationServletContextListener.java
@@ -18,45 +18,61 @@ import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.sql.DataSource;
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Properties;
 
 public class InitializationServletContextListener implements ServletContextListener {
 
-    public static final String QUERY_SELECT_PROFILE_BY_LOGIN_AND_PASSWORD = "query.select.profile.byLoginAndPassword";
-    public static final String QUERY_SELECT_PROFILE_ROLES_BY_PROFILE_LOGIN = "query.select.profileRoles.byProfileLogin";
-    public static final String ALIAS_PROFILE_ROLE_OF_PROFILE = "alias.profile.roleOfProfile";
-
-    public static final String ALIAS_LANGUAGE_ID = "alias.language.id";
-    public static final String ALIAS_SUBJECT_ID = "alias.subject.id";
-    public static final String QUERY_SELECT_LANGUAGE_BY_ID = "query.select.languageById";
-    public static final String QUERY_INSERT_SUBJECT = "query.insert.subject";
-    public static final String QUERY_INSERT_EXAM = "query.insert.exam";
-    public static final String QUERY_INSERT_QUESTION = "query.insert.question";
-    public static final String QUERY_INSERT_ANSWER = "query.insert.answer";
-    public static final String QUERY_SELECT_SUBJECT_BY_ID = "query.select.subjectById";
-    public static final String COLUMN_SUBJECT_ID = "column.subject.id";
-    public static final String COLUMN_EXAM_ID = "column.exam.id";
-    public static final String COLUMN_QUESTION_ID = "column.question.id";
-    public static final String COLUMN_ANSWER_ID = "column.answer.id";
-    public static final String ALIAS_EXAM_ID = "alias.exam.id";
-    public static final String ALIAS_SUBJECT_NAME = "alias.subject.name";
-    public static final String ALIAS_EXAM_NAME = "alias.exam.name";
-    public static final String QUERY_SELECT_EXAMS_WITHOUT_QUESTIONS = "query.select.exams.withoutQuestions";
-    public static final String ALIAS_QUESTION_ID = "alias.question.id";
-    public static final String ALIAS_QUESTION_TEXT = "alias.question.text";
-    public static final String ALIAS_ANSWER_ID = "alias.answer.id";
-    public static final String ALIAS_ANSWER_TEXT = "alias.answer.text";
-    public static final String ALIAS_ANSWER_IS_CORRECT = "alias.answer.isCorrect";
-    public static final String QUERY_SELECT_EXAM_BY_ID = "query.select.examById";
+    private static final String QUERY_SELECT_PROFILE_BY_LOGIN_AND_PASSWORD = "query.select.profile.byLoginAndPassword";
+    private static final String QUERY_SELECT_PROFILE_ROLES_BY_PROFILE_LOGIN = "query.select.profileRoles.byProfileLogin";
+    private static final String ALIAS_PROFILE_ROLE_OF_PROFILE = "alias.profile.roleOfProfile";
+    private static final String ALIAS_LANGUAGE_ID = "alias.language.id";
+    private static final String ALIAS_SUBJECT_ID = "alias.subject.id";
+    private static final String QUERY_SELECT_LANGUAGE_BY_ID = "query.select.languageById";
+    private static final String QUERY_INSERT_SUBJECT = "query.insert.subject";
+    private static final String QUERY_INSERT_EXAM = "query.insert.exam";
+    private static final String QUERY_INSERT_QUESTION = "query.insert.question";
+    private static final String QUERY_INSERT_ANSWER = "query.insert.answer";
+    private static final String QUERY_SELECT_SUBJECT_BY_ID = "query.select.subjectById";
+    private static final String COLUMN_SUBJECT_ID = "column.subject.id";
+    private static final String COLUMN_EXAM_ID = "column.exam.id";
+    private static final String COLUMN_QUESTION_ID = "column.question.id";
+    private static final String COLUMN_ANSWER_ID = "column.answer.id";
+    private static final String ALIAS_EXAM_ID = "alias.exam.id";
+    private static final String ALIAS_SUBJECT_NAME = "alias.subject.name";
+    private static final String ALIAS_EXAM_NAME = "alias.exam.name";
+    private static final String QUERY_SELECT_EXAMS_WITHOUT_QUESTIONS = "query.select.exams.withoutQuestions";
+    private static final String ALIAS_QUESTION_ID = "alias.question.id";
+    private static final String ALIAS_QUESTION_TEXT = "alias.question.text";
+    private static final String ALIAS_ANSWER_ID = "alias.answer.id";
+    private static final String ALIAS_ANSWER_TEXT = "alias.answer.text";
+    private static final String ALIAS_ANSWER_IS_CORRECT = "alias.answer.isCorrect";
+    private static final String QUERY_SELECT_EXAM_BY_ID = "query.select.examById";
     private static final String QUERY_SELECT_SUBJECTS_BY_LANGUAGE_CODE = "query.select.subject.byLanguageCode";
-    private final static Logger logger = LogManager.getLogger(InitializationServletContextListener.class);
+
+    private static final Logger logger = LogManager.getLogger(InitializationServletContextListener.class);
 
     @Override
     public void contextInitialized(ServletContextEvent servletContextEvent) {
+        logger.info("Starting application initialization");
+
         ServletContext context = servletContextEvent.getServletContext();
 
+        Properties sqlStrings = new Properties();
+        try {
+            sqlStrings.load(Thread.currentThread().getContextClassLoader().getResourceAsStream(context.getInitParameter("sql_strings")));
+        } catch (IOException e) {
+            logger.fatal("Unable to load SQL strings from file {}", context.getInitParameter("sql_strings"));
+            throw new SphinxInitializationException(e);
+        }
+
+        logger.info("SQL strings loaded");
+        for (Map.Entry<Object, Object> s : sqlStrings.entrySet()) {
+            logger.info("SQL string '{}' has value '{}'", s.getKey() , s.getValue());
+        }
 
         context.setAttribute("examSubmissionMapper", new ExamSubmissionMapper());
         context.setAttribute("examCreationMapper", new ExamCreationMapper());
@@ -79,47 +95,16 @@ public class InitializationServletContextListener implements ServletContextListe
         Map<String, String> rolesMapping = new HashMap<String, String>();
         rolesMapping.put("tutor", "tutor");
         rolesMapping.put("student", "student");
-
-        //TODO retrieving sql strings from file
-        HashMap<String, String> sqlStringsMap = new HashMap<>();
-        sqlStringsMap.put(QUERY_SELECT_PROFILE_BY_LOGIN_AND_PASSWORD, "SELECT LOGIN FROM PROFILE WHERE LOGIN LIKE ? AND PASSWORD LIKE ?");
-        sqlStringsMap.put(QUERY_SELECT_PROFILE_ROLES_BY_PROFILE_LOGIN, "SELECT R.NAME AS ROLE_OF_PROFILE FROM ROLE R JOIN PROFILE_ROLE PR ON PR.ROLE_ID = R.ID JOIN PROFILE P ON P.ID = PR.PROFILE_ID WHERE P.LOGIN LIKE ?");
-        sqlStringsMap.put(ALIAS_PROFILE_ROLE_OF_PROFILE, "ROLE_OF_PROFILE");
-
-
-        sqlStringsMap.put(ALIAS_LANGUAGE_ID, "LANGUAGE_ID");
-        sqlStringsMap.put(ALIAS_SUBJECT_ID, "SUBJECT_ID");
-        sqlStringsMap.put(QUERY_SELECT_LANGUAGE_BY_ID, "SELECT ID AS LANGUAGE_ID FROM LANGUAGE WHERE CODE LIKE ?");
-        sqlStringsMap.put(QUERY_INSERT_SUBJECT, "INSERT INTO SUBJECT (NAME, LANGUAGE_ID) VALUES(?,?)");
-        sqlStringsMap.put(QUERY_INSERT_EXAM, "INSERT INTO EXAM (SUBJECT_ID, NAME) VALUES(?,?)");
-        sqlStringsMap.put(QUERY_INSERT_QUESTION, "INSERT INTO QUESTION (EXAM_ID, TEXT) VALUES(?,?)");
-        sqlStringsMap.put(QUERY_INSERT_ANSWER, "INSERT INTO ANSWER (QUESTION_ID, TEXT, IS_CORRECT) VALUES(?,?,?)");
-        sqlStringsMap.put(QUERY_SELECT_SUBJECT_BY_ID, "SELECT ID AS SUBJECT_ID FROM SUBJECT WHERE NAME LIKE ?");
-        sqlStringsMap.put(COLUMN_SUBJECT_ID, "ID");
-        sqlStringsMap.put(COLUMN_EXAM_ID, "ID");
-        sqlStringsMap.put(COLUMN_QUESTION_ID, "ID");
-        sqlStringsMap.put(COLUMN_ANSWER_ID, "ID");
-        sqlStringsMap.put(ALIAS_EXAM_ID, "EXAM_ID");
-        sqlStringsMap.put(ALIAS_SUBJECT_NAME, "SUBJECT_NAME");
-        sqlStringsMap.put(ALIAS_EXAM_NAME, "EXAM_NAME");
-        sqlStringsMap.put(QUERY_SELECT_EXAMS_WITHOUT_QUESTIONS, "SELECT EXAM.ID AS EXAM_ID, EXAM.NAME AS EXAM_NAME, SUBJECT.NAME AS SUBJECT_NAME FROM EXAM JOIN SUBJECT ON SUBJECT.ID = EXAM.SUBJECT_ID");
-        sqlStringsMap.put(ALIAS_QUESTION_ID, "QUESTION_ID");
-        sqlStringsMap.put(ALIAS_QUESTION_TEXT, "QUESTION_TEXT");
-        sqlStringsMap.put(ALIAS_ANSWER_ID, "ANSWER_ID");
-        sqlStringsMap.put(ALIAS_ANSWER_TEXT, "ANSWER_TEXT");
-        sqlStringsMap.put(ALIAS_ANSWER_IS_CORRECT, "ANSWER_IS_CORRECT");
-        sqlStringsMap.put(QUERY_SELECT_EXAM_BY_ID, "SELECT EXAM.ID AS EXAM_ID, EXAM.NAME AS EXAM_NAME, SUBJECT.NAME AS SUBJECT_NAME, QUESTION.ID AS QUESTION_ID, QUESTION.TEXT AS QUESTION_TEXT, ANSWER.ID AS ANSWER_ID, ANSWER.TEXT AS ANSWER_TEXT, ANSWER.IS_CORRECT AS ANSWER_IS_CORRECT FROM EXAM JOIN SUBJECT ON EXAM.SUBJECT_ID = SUBJECT.ID JOIN QUESTION ON EXAM.ID = QUESTION.EXAM_ID JOIN ANSWER ON QUESTION.ID = ANSWER.QUESTION_ID WHERE EXAM.ID = ?");
-        sqlStringsMap.put(QUERY_SELECT_SUBJECTS_BY_LANGUAGE_CODE, "SELECT SUBJECT.ID AS SUBJECT_ID, SUBJECT.NAME AS SUBJECT_NAME FROM SUBJECT JOIN LANGUAGE ON LANGUAGE.ID = SUBJECT.LANGUAGE_ID WHERE LANGUAGE.CODE LIKE ?");
-
+        
 
 /*        HashMap sqlStringHolder = new HashMap();
         OR ever object?*/
 
 
         ProfileDaoImpl profileDaoImpl = new ProfileDaoImpl(dataSource, rolesMapping);
-        profileDaoImpl.setQuerySelectProfileByLoginAndPassword(sqlStringsMap.get(QUERY_SELECT_PROFILE_BY_LOGIN_AND_PASSWORD));
-        profileDaoImpl.setQuerySelectProfileRolesByProfileLogin(sqlStringsMap.get(QUERY_SELECT_PROFILE_ROLES_BY_PROFILE_LOGIN));
-        profileDaoImpl.setAliasProfileRoleOfProfile(sqlStringsMap.get(ALIAS_PROFILE_ROLE_OF_PROFILE));
+        profileDaoImpl.setQuerySelectProfileByLoginAndPassword(sqlStrings.getProperty(QUERY_SELECT_PROFILE_BY_LOGIN_AND_PASSWORD));
+        profileDaoImpl.setQuerySelectProfileRolesByProfileLogin(sqlStrings.getProperty(QUERY_SELECT_PROFILE_ROLES_BY_PROFILE_LOGIN));
+        profileDaoImpl.setAliasProfileRoleOfProfile(sqlStrings.getProperty(ALIAS_PROFILE_ROLE_OF_PROFILE));
 
         context.setAttribute("profileDao", profileDaoImpl);
 
@@ -129,14 +114,14 @@ public class InitializationServletContextListener implements ServletContextListe
 
         SubjectDao subjectDao;
         SubjectDaoImpl subjectDaoImpl = new SubjectDaoImpl(dataSource);
-        subjectDaoImpl.setAliasLanguageId(sqlStringsMap.get(ALIAS_LANGUAGE_ID));
-        subjectDaoImpl.setAliasSubjectId(sqlStringsMap.get(ALIAS_SUBJECT_ID));
-        subjectDaoImpl.setAliasSubjectName(sqlStringsMap.get(ALIAS_SUBJECT_NAME));
-        subjectDaoImpl.setQuerySelectLanguageById(sqlStringsMap.get(QUERY_SELECT_LANGUAGE_BY_ID));
-        subjectDaoImpl.setQueryInsertSubject(sqlStringsMap.get(QUERY_INSERT_SUBJECT));
-        subjectDaoImpl.setQuerySelectSubjectById(sqlStringsMap.get(QUERY_SELECT_SUBJECT_BY_ID));
-        subjectDaoImpl.setColumnSubjectId(sqlStringsMap.get(COLUMN_SUBJECT_ID));
-        subjectDaoImpl.setQuerySelectSubjectsByLanguageCode(sqlStringsMap.get(QUERY_SELECT_SUBJECTS_BY_LANGUAGE_CODE));
+        subjectDaoImpl.setAliasLanguageId(sqlStrings.getProperty(ALIAS_LANGUAGE_ID));
+        subjectDaoImpl.setAliasSubjectId(sqlStrings.getProperty(ALIAS_SUBJECT_ID));
+        subjectDaoImpl.setAliasSubjectName(sqlStrings.getProperty(ALIAS_SUBJECT_NAME));
+        subjectDaoImpl.setQuerySelectLanguageById(sqlStrings.getProperty(QUERY_SELECT_LANGUAGE_BY_ID));
+        subjectDaoImpl.setQueryInsertSubject(sqlStrings.getProperty(QUERY_INSERT_SUBJECT));
+        subjectDaoImpl.setQuerySelectSubjectById(sqlStrings.getProperty(QUERY_SELECT_SUBJECT_BY_ID));
+        subjectDaoImpl.setColumnSubjectId(sqlStrings.getProperty(COLUMN_SUBJECT_ID));
+        subjectDaoImpl.setQuerySelectSubjectsByLanguageCode(sqlStrings.getProperty(QUERY_SELECT_SUBJECTS_BY_LANGUAGE_CODE));
 
         subjectDao = subjectDaoImpl;
 
@@ -148,22 +133,22 @@ public class InitializationServletContextListener implements ServletContextListe
         ExamDao examDao;
         ExamDaoImpl examDaoImpl = new ExamDaoImpl(dataSource);
 
-        examDaoImpl.setQueryInsertExam(sqlStringsMap.get(QUERY_INSERT_EXAM));
-        examDaoImpl.setQueryInsertQuestion(sqlStringsMap.get(QUERY_INSERT_QUESTION));
-        examDaoImpl.setQueryInsertAnswer(sqlStringsMap.get(QUERY_INSERT_ANSWER));
-        examDaoImpl.setColumnExamId(sqlStringsMap.get(COLUMN_EXAM_ID));
-        examDaoImpl.setColumnQuestionId(sqlStringsMap.get(COLUMN_QUESTION_ID));
-        examDaoImpl.setColumnAnswerId(sqlStringsMap.get(COLUMN_ANSWER_ID));
-        examDaoImpl.setAliasExamId(sqlStringsMap.get(ALIAS_EXAM_ID));
-        examDaoImpl.setAliasSubjectName(sqlStringsMap.get(ALIAS_SUBJECT_NAME));
-        examDaoImpl.setAliasExamName(sqlStringsMap.get(ALIAS_EXAM_NAME));
-        examDaoImpl.setQuerySelectExamsWithoutQuestions(sqlStringsMap.get(QUERY_SELECT_EXAMS_WITHOUT_QUESTIONS));
-        examDaoImpl.setAliasQuestionId(sqlStringsMap.get(ALIAS_QUESTION_ID));
-        examDaoImpl.setAliasQuestionText(sqlStringsMap.get(ALIAS_QUESTION_TEXT));
-        examDaoImpl.setAliasAnswerId(sqlStringsMap.get(ALIAS_ANSWER_ID));
-        examDaoImpl.setAliasAnswerText(sqlStringsMap.get(ALIAS_ANSWER_TEXT));
-        examDaoImpl.setAliasAnswerIsCorrect(sqlStringsMap.get(ALIAS_ANSWER_IS_CORRECT));
-        examDaoImpl.setQuerySelectExamById(sqlStringsMap.get(QUERY_SELECT_EXAM_BY_ID));
+        examDaoImpl.setQueryInsertExam(sqlStrings.getProperty(QUERY_INSERT_EXAM));
+        examDaoImpl.setQueryInsertQuestion(sqlStrings.getProperty(QUERY_INSERT_QUESTION));
+        examDaoImpl.setQueryInsertAnswer(sqlStrings.getProperty(QUERY_INSERT_ANSWER));
+        examDaoImpl.setColumnExamId(sqlStrings.getProperty(COLUMN_EXAM_ID));
+        examDaoImpl.setColumnQuestionId(sqlStrings.getProperty(COLUMN_QUESTION_ID));
+        examDaoImpl.setColumnAnswerId(sqlStrings.getProperty(COLUMN_ANSWER_ID));
+        examDaoImpl.setAliasExamId(sqlStrings.getProperty(ALIAS_EXAM_ID));
+        examDaoImpl.setAliasSubjectName(sqlStrings.getProperty(ALIAS_SUBJECT_NAME));
+        examDaoImpl.setAliasExamName(sqlStrings.getProperty(ALIAS_EXAM_NAME));
+        examDaoImpl.setQuerySelectExamsWithoutQuestions(sqlStrings.getProperty(QUERY_SELECT_EXAMS_WITHOUT_QUESTIONS));
+        examDaoImpl.setAliasQuestionId(sqlStrings.getProperty(ALIAS_QUESTION_ID));
+        examDaoImpl.setAliasQuestionText(sqlStrings.getProperty(ALIAS_QUESTION_TEXT));
+        examDaoImpl.setAliasAnswerId(sqlStrings.getProperty(ALIAS_ANSWER_ID));
+        examDaoImpl.setAliasAnswerText(sqlStrings.getProperty(ALIAS_ANSWER_TEXT));
+        examDaoImpl.setAliasAnswerIsCorrect(sqlStrings.getProperty(ALIAS_ANSWER_IS_CORRECT));
+        examDaoImpl.setQuerySelectExamById(sqlStrings.getProperty(QUERY_SELECT_EXAM_BY_ID));
 
         examDao = examDaoImpl;
         ExamServiceImpl examService = new ExamServiceImpl();
